4f8f4cf72fb4f9335113c134633c7012455244f5
Saša Barišić <sbarisic@vub.hr>
2025-01-12 09:15:40 +0100

    Update Added visual studio config

--- winget_install.bat
+++ winget_install.bat
@@ -19,7 +19,7 @@ REM Programs
 winget install -e --id 7zip.7zip
 winget install -e --id Notepad++.Notepad++
 winget install -e --id Mozilla.Firefox
-winget install -e --id Postman.Postman
+REM winget install -e --id Postman.Postman
 winget install -e --id Spotify.Spotify
 winget install -e --id VideoLAN.VLC
 winget install -e --id Axosoft.GitKraken
@@ -30,8 +30,12 @@ winget install -e --id ShareX.ShareX
 winget install -e --id Discord.Discord
 winget install -e --id Logitech.GHUB
 winget install -e --id BaldurKarlsson.RenderDoc
+winget install -e --id Modrinth.ModrinthApp
+winget install -e --id Rakuten.Viber
+winget install -e --id Valve.Steam
+winget install -e --id Microsoft.VisualStudio.2022.Community
 
 echo Copying local settings for programs
 REM Settings
 taskkill /im sharex.exe /f
-robocopy "./Sharex" "%userprofile%/Documents/ShareX" /is /it
+robocopy "./MyDocuments/Sharex" "%userprofile%/Documents/ShareX" /is /it
